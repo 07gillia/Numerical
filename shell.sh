d94a826fc511b859294184626e3d5bcfe2f0244a
Alex Gillies <ColossusMini@alexander.dur.ac.uk>
2016-11-16 16:03:46 +0000

    compiling doesn’t work in slurm so its being done in a shell script

---
+++ shell.sh
@@ -0,0 +1,25 @@
+
+
+icpc -O3 particles_10.c -o particle_10
+
+icpc -O3 particles_100.c -o particle_100
+
+icpc -O3 particles_1000.c -o particle_1000
+
+icpc -O3 particles_10000.c -o particle_10000
+
+icpc -O3 particles_100000.c -o particle_100000
+
+icpc -O3 particles_1000000.c -o particle_1000000
+
+icpc -O3 particles_adva_10.c -o particle_adva_10
+
+icpc -O3 particles_adva_100.c -o particle_adva_100
+
+icpc -O3 particles_adva_1000.c -o particle_adva_1000
+
+icpc -O3 particles_adva_10000.c -o particle_adva_10000
+
+icpc -O3 particles_adva_100000.c -o particle_adva_100000
+
+icpc -O3 particles_adva_1000000.c -o particle_adva_1000000
