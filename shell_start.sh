e64d9b55f594dee0cd626f04490812b5b1d986f6
Alex Gillies <ColossusMini@alexander.dur.ac.uk>
2016-11-18 16:06:09 +0000

    I have no idea what just happened, particles_adva_27_10

--- shell_start.sh
+++ shell_start.sh
@@ -2,26 +2,50 @@ module purge
 module load slurm
 module load intel/xe_2015.2
 
-icpc -O3 particles_10.c -o particle_10
+icpc -O3 particles_7_10.c -o particle_7_10
 
-icpc -O3 particles_100.c -o particle_100
+icpc -O3 particles_7_100.c -o particle_7_100
 
-icpc -O3 particles_1000.c -o particle_1000
+icpc -O3 particles_7_1000.c -o particle_7_1000
 
-icpc -O3 particles_10000.c -o particle_10000
+icpc -O3 particles_7_10000.c -o particle_7_10000
 
-icpc -O3 particles_100000.c -o particle_100000
+icpc -O3 particles_7_100000.c -o particle_7_100000
 
-icpc -O3 particles_1000000.c -o particle_1000000
+icpc -O3 particles_7_1000000.c -o particle_7_1000000
 
-icpc -O3 particles_adva_10.c -o particle_adva_10
+icpc -O3 particles_adva_7_10.c -o particle_7_adva_10
 
-icpc -O3 particles_adva_100.c -o particle_adva_100
+icpc -O3 particles_adva_7_100.c -o particle_adva_7_100
 
-icpc -O3 particles_adva_1000.c -o particle_adva_1000
+icpc -O3 particles_adva_7_1000.c -o particle_adva_7_1000
 
-icpc -O3 -mcmodel=large particles_adva_10000.c -o particle_adva_10000
+icpc -O3 -mcmodel=large particles_adva_7_10000.c -o particle_adva_7_10000
 
-icpc -O3 -mcmodel=large particles_adva_100000.c -o particle_adva_100000
+icpc -O3 -mcmodel=large particles_adva_7_100000.c -o particle_adva_7_100000
 
-icpc -O3 -mcmodel=large particles_adva_1000000.c -o particle_adva_1000000
+icpc -O3 -mcmodel=large particles_adva_7_1000000.c -o particle_adva_7_1000000
+
+icpc -O3 particles_27_10.c -o particle_27_10
+
+icpc -O3 particles_27_100.c -o particle_27_100
+
+icpc -O3 particles_27_1000.c -o particle_27_1000
+
+icpc -O3 particles_27_10000.c -o particle_27_10000
+
+icpc -O3 particles_27_100000.c -o particle_27_100000
+
+icpc -O3 particles_27_1000000.c -o particle_27_1000000
+
+icpc -O3 particles_adva_27_10.c -o particle_27_adva_10
+
+icpc -O3 particles_adva_27_100.c -o particle_adva_27_100
+
+icpc -O3 particles_adva_27_1000.c -o particle_adva_27_1000
+
+icpc -O3 -mcmodel=large particles_adva_27_10000.c -o particle_adva_27_10000
+
+icpc -O3 -mcmodel=large particles_adva_27_100000.c -o particle_adva_27_100000
+
+icpc -O3 -mcmodel=large particles_adva_27_1000000.c -o particle_adva_27_1000000
