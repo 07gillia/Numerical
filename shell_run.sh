f50df78fb8f8a9549001da4cce8d0d8608b399e3
Alex Gillies <ColossusMini@alexander.dur.ac.uk>
2016-11-25 13:13:39 +0000

    updated slurm stuff

--- shell_run.sh
+++ shell_run.sh
@@ -13,17 +13,17 @@ sbatch numerical_1_27_100000.slurm-script
 
 sbatch numerical_1_27_1000000.slurm-script
 
-sbatch numerical_3_27_10.slurm-script
+sbatch numerical_adva_1_27_10.slurm-script
 
-sbatch numerical_3_27_100.slurm-script
+sbatch numerical_adva_1_27_100.slurm-script
 
-sbatch numerical_3_27_1000.slurm-script
+sbatch numerical_adva_1_27_1000.slurm-script
 
-sbatch numerical_3_27_10000.slurm-script
+sbatch numerical_adva_1_27_10000.slurm-script
 
-sbatch numerical_3_27_100000.slurm-script
+sbatch numerical_adva_1_27_100000.slurm-script
 
-sbatch numerical_3_27_1000000.slurm-script
+sbatch numerical_adva_1_27_1000000.slurm-script
 
 sbatch numerical_1_7_10.slurm-script
 
@@ -37,14 +37,14 @@ sbatch numerical_1_7_100000.slurm-script
 
 sbatch numerical_1_7_1000000.slurm-script
 
-sbatch numerical_3_7_10.slurm-script
+sbatch numerical_adva_1_7_10.slurm-script
 
-sbatch numerical_3_7_100.slurm-script
+sbatch numerical_adva_1_7_100.slurm-script
 
-sbatch numerical_3_7_1000.slurm-script
+sbatch numerical_adva_1_7_1000.slurm-script
 
-sbatch numerical_3_7_10000.slurm-script
+sbatch numerical_adva_1_7_10000.slurm-script
 
-sbatch numerical_3_7_100000.slurm-script
+sbatch numerical_adva_1_7_100000.slurm-script
 
-sbatch numerical_3_7_1000000.slurm-script
+sbatch numerical_adva_1_7_1000000.slurm-script
