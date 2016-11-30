1161b5a6eccc6d5a707237c0db953b8613279bb8
Alex Gillies <ColossusMini@alexander.dur.ac.uk>
2016-11-30 11:47:54 +0000

    uploaded.

---
+++ overnight.sh
@@ -0,0 +1,59 @@
+g++ -O3 particles_7_10.c -o particles_7_10
+./particles_7_10
+
+g++ -O3 particles_7_100.c -o particles_7_100
+./particles_7_100
+
+g++ -O3 particles_7_1000.c -o particles_7_1000
+./particles_7_1000
+
+g++ -O3 particles_7_10000.c -o particles_7_10000
+./particles_7_10000
+
+g++ -O3 particles_7_100000.c -o particles_7_100000
+./particles_7_100000
+
+g++ -O3 particles_27_10.c -o particles_27_10
+./particles_27_10
+
+g++ -O3 particles_27_100.c -o particles_27_100
+./particles_27_100
+
+g++ -O3 particles_27_1000.c -o particles_27_1000
+./particles_27_1000
+
+g++ -O3 particles_27_10000.c -o particles_27_10000
+./particles_27_10000
+
+g++ -O3 particles_27_100000.c -o particles_27_100000
+./particles_27_100000
+
+g++ -O3 particles_adva_7_10.c -o particles_adva_7_10
+./particles_adva_7_10
+
+g++ -O3 particles_adva_7_100.c -o particles_adva_7_100
+./particles_adva_7_100
+
+g++ -O3 particles_adva_7_1000.c -o particles_adva_7_1000
+./particles_adva_7_1000
+
+g++ -O3 particles_adva_7_10000.c -o particles_adva_7_10000
+./particles_adva_7_10000
+
+g++ -O3 particles_adva_7_100000.c -o particles_adva_7_100000
+./particles_adva_7_100000
+
+g++ -O3 particles_adva_27_10.c -o particles_adva_27_10
+./particles_adva_27_10
+
+g++ -O3 particles_adva_27_100.c -o particles_adva_27_100
+./particles_adva_27_100
+
+g++ -O3 particles_adva_27_1000.c -o particles_adva_27_1000
+./particles_adva_27_1000
+
+g++ -O3 particles_adva_27_10000.c -o particles_adva_27_10000
+./particles_adva_27_10000
+
+g++ -O3 particles_adva_27_100000.c -o particles_adva_27_100000
+./particles_adva_27_100000
